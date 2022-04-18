3f843adfc82972cb08310900f7f94de21af9783c
Kern Jacobo <jacobokern5@gmail.com>
2022-04-19 02:35:53 +0800

    bug trouble population logic corrected

--- first-year/second-sem/midterms/activity-2/kpsjacobomidtermactivity2bugtrouble.java
+++ first-year/second-sem/midterms/activity-2/kpsjacobomidtermactivity2bugtrouble.java
@@ -12,7 +12,7 @@ public class kpsjacobomidtermactivity2bugtrouble
         Scanner stdin = new Scanner(System.in);
         
         // ask for the house volume
-        System.out.printf("Enter house volume (in cubic meter): ");
+        System.out.printf("Enter house volume (in cubic feet): ");
         double houseVolume = Double.parseDouble(stdin.nextLine());
         
         // ask for estimated roach population
@@ -24,17 +24,15 @@ public class kpsjacobomidtermactivity2bugtrouble
         int countWeeks = 0;
         while (totalBugVolume < houseVolume) 
         {   
-            double newBugs = population * GROWTH_RATE; // compute new roach population
-            double newBugVolume = population * ONE_BUG_VOLUME; // compute new roach volume
-            population += newBugs; // add it to the total population
-            totalBugVolume += newBugVolume; // add it to the total volume
-            ++countWeeks; // increment week
+            population += (GROWTH_RATE * population);
+            totalBugVolume = population * ONE_BUG_VOLUME;
+            ++countWeeks;
         }
 
         // print out results
         System.out.printf("\nResults after %d weeks\n", countWeeks);
         System.out.printf("Roaches: %d\n", (int)population);
-        System.out.printf("Volume filled by roaches (in cubic meter): %.2f\n", totalBugVolume);
+        System.out.printf("Volume filled by roaches (in cubic feet): %.2f\n", totalBugVolume);
 
         // Quit the app
         stdin.close();
