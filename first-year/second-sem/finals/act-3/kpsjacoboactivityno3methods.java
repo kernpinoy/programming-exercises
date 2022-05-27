9b306ad1c4218b568916a280f7c818cee4803692
Kern Jacobo <jacobokern5@gmail.com>
2022-05-27 22:34:19 +0800

    added Activity 3 finals main

---
+++ first-year/second-sem/finals/act-3/kpsjacoboactivityno3methods.java
@@ -0,0 +1,9 @@
+class kpsjacoboactivityno3methods
+{ 
+    public static void main(String[] args) 
+    {
+        kpsjacoboactivity3class cruiser = new kpsjacoboactivity3class();
+        
+        cruiser.start();
+    }
+}
